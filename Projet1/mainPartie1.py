ec1df8ebe3dae202d7a1d67a24fecebfc922e7b4
mprTest1 <98311505+mprTest1@users.noreply.github.com>
2022-09-29 15:40:10 +0200

    partie2

--- Projet1/mainPartie1.py
+++ Projet1/mainPartie1.py
@@ -16,7 +16,7 @@ class Grille:
         #Numéroter la grille de gauche à droite de haut en bas, en utilisant les chiffres de 0 à 9
         self.grille = numpy.zeros((10, 10))
 
-    def bat_longueur(self, bateau):
+    def bat_longueur(bateau):
         """
         Définir la taille de chaque type de bateau et retourne le longueur
         """
@@ -33,7 +33,7 @@ class Grille:
         
         return length
 
-    def peut_placer(self, grille, bateau, position, direction):
+    def peut_placer(grille, bateau, position, direction):
         """
         retourne True si il est possible de placer la bateau et tous, False sinon
         """
@@ -43,22 +43,22 @@ class Grille:
             return False
 
         # calculer le longueur de bateau
-        length = self.bat_longueur(bateau)
+        length = Grille.bat_longueur(bateau)
         if length==0:
             print("Err : wrong ship type number !")
             return False
 
         if direction==1:
-           if position[0]+length<9:
+           if position[0]+length<11:
                 for i in range(position[0], position[0]+length):
-                    if self.grille[i][position[1]] != 0:
+                    if grille[i][position[1]] != 0:
                        return False
                 return True
         
         elif direction==2:
-           if position[1]+length<9:
+           if position[1]+length<11:
                 for i in range(position[1], position[1]+length):
-                    if self.grille[position[0]][i] != 0:
+                    if grille[position[0]][i] != 0:
                        return False
                 return True
         
@@ -70,10 +70,10 @@ class Grille:
         """
         
         #vérifier la opération est vailde à l'avance et puis on continue d'insertion de bateau
-        if not self.peut_placer(grille, bateau, position, direction):
+        if not Grille.peut_placer(grille, bateau, position, direction):
             return False
         
-        length = self.bat_longueur(bateau)
+        length = Grille.bat_longueur(bateau)
 
         if direction == 1:
             for i in range(position[0], position[0]+length):
@@ -101,7 +101,7 @@ class Grille:
         Effectuer une boucle jusqu'à ce que le navire soit inséré avec succès
         """
         pos,dir = self.generer_position()
-        while not self.peut_placer(grille, bateau, pos, dir):
+        while not Grille.peut_placer(grille, bateau, pos, dir):
             pos,dir = self.generer_position()
         self.place(self.grille,bateau,pos,dir)
     
@@ -111,7 +111,7 @@ class Grille:
         ax2.imshow(grille, interpolation='nearest', cmap=cm.Greys_r)
         plt.show()
     
-    def eq(self, grilleA, grilleB):
+    def eq(grilleA, grilleB):
         """
         retourne True si grilleA == grilleB (tous les points des deux matrice sont éqals), false sinon
         """
@@ -125,9 +125,63 @@ class Grille:
             self.place_alea(self.grille,i)
 
 #les codes pour tester les fonctions dans le ficher mainPartie1.py
+#g0 = Grille()         #la création de la grille 
+#g0.genere_grille()
+#g0.affiche(g0.grille)
 
-g0 = Grille()         #la création de la grille 
-g0.genere_grille()
-g0.affiche(g0.grille)
+def count_possible(bateau):
+    """
+    permet de calculer le nombre de façons de placer un bateau donné sur une grille vide.
+    """
+    gTemp=Grille()
+    count=0
+    for i in range(0,10):
+        for j in range(0,10):
+            for d in range(1,3):
+                if Grille.peut_placer(gTemp.grille, bateau, (i,j), d):
+                    count+=1
+    return count
 
+def count_possible_mult(list_bateau):
+    """
+    permet de calculer le nombre de façon de placer une liste de bateaux sur une grille vide
+    """
+    gTemp=Grille()
+    return count_possible_rec(gTemp.grille, list_bateau)
+    
+def count_possible_rec(grille, list_bateau):
+    if len(list_bateau)==0:
+        return 1
+    count=0
+    gTmp=Grille()
+    for d in range(1,3):
+        for i in range(0,10):
+            for j in range(0,10):
+                if Grille.peut_placer(grille,list_bateau[0],(i,j),d):
+                    #print(str(i)+" "+str(j)+" "+str(d))
+                    gTmp.grille=grille.copy()
+                    gTmp.place(gTmp.grille,list_bateau[0],(i,j),d)
+                    count+=count_possible_rec(gTmp.grille, list_bateau[1:])
+    return count
 
+print(count_possible(3))
+print(count_possible_mult([1]))
+print(count_possible_mult([1,2]))
+print(count_possible_mult([1,2,3]))
+
+def generer_grille_equiproque(grille):
+    g0=Grille()
+    count=0
+    while not Grille.eq(grille,g0.grille):
+        g0.grille=numpy.zeros((10, 10))
+        g0.genere_grille()
+        count+=1
+    return count
+
+def approximation(precision):
+    g0=Grille()
+    g0.genere_grille()
+    avg=0
+    for i in range(precision):
+        avg+=generer_grille_equiproque(g0.grille)
+    return avg/precision

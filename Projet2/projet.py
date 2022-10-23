ed759dce513c6e72b96c72bd2417435dfe3cc403
RUNLIN ZHOU <72131094+ZhouRUNLIN@users.noreply.github.com>
2022-10-23 15:27:57 +0200

    Add files via upload

--- Projet2/projet.py
+++ Projet2/projet.py
@@ -4,17 +4,21 @@ import utils
 import math
 
 def getPrior(data):
-    N = len(data['target'])
+    N = 0
     N1 = 0
     for i in data['target']:
+        N += 1
         if i == 1:
             N1 += 1
 
-    estimation = N1/N
-    max5pourcent = estimation + 2*math.sqrt((estimation*(1 - estimation))/N)
-    min5pourcent = estimation - 2*math.sqrt((estimation*(1 - estimation))/N)
+    estimation = N1/N           #la valeur de p
+    variance = math.sqrt(estimation*(1 - estimation)/N) 
+    min5pourcent = estimation - 1.96*variance
+    max5pourcent = estimation + 1.96*variance
 
     return{"estimation": estimation,"min5pourcent": min5pourcent, "max5pourcent": max5pourcent}
+
+
 
 class APrioriClassifier(utils.AbstractClassifier):
   """
@@ -32,36 +36,43 @@ class APrioriClassifier(utils.AbstractClassifier):
     Afin de faciliter la comparaison de la relation, nous utilisons la fonction logarithmique et utilisons log(lift) comme indicateur de relation entre l'entité et la cible.
     La fonction génère un dictionnaire avec chaque caractéristique encodée et son index de relation sous forme de paires clé-valeur.
     """
+    
     dict0=dict()
     train0=pd.read_csv("train.csv")
+    
     for i in train0.keys():
         if i != "target":
             train0[i]=train0[i].astype(str)
+            
     encoded_train=pd.get_dummies(train0)
     pClass1=getPrior(train0)["estimation"]
     dataClass1=encoded_train.groupby("target").get_group(1)
+    
     for j in encoded_train.keys():
         if j != "target":
             pJClass1=max(dataClass1.sum()[j],1)/dataClass1.count()[j]
             pJ=encoded_train.sum()[j]/encoded_train.count()[j]
             dict0[j]=math.log(pJClass1/(pJ*pClass1))
+            
     return dict0
     
   def estimClass(self, attrs):
     """
     à partir d'un dictionanire d'attributs, estime la classe 0 ou 1
 
-    :param attrs: le  dictionnaire nom-valeur des attributs
+    :paramètres: le dictionnaire 'nom-valeur' des attributs
     :return: la classe 0 ou 1 estimée
     """
     s=0.0
-    model=self.train_model()
+    
+    model = self.train_model()
     for i in attrs.keys():
-        tempKey=i+"_"+str(attrs[i])
+        tempKey = i + "_" + str(attrs[i])
         if tempKey in model:
-            s+=model[tempKey]
+            s += model[tempKey]
     if s>0:
         return 1
+    
     return 0
 
   def statsOnDF(self, df):
@@ -72,23 +83,28 @@ class APrioriClassifier(utils.AbstractClassifier):
     FP : nombre d'individus avec target=0 et classe prévue=1
     FN : nombre d'individus avec target=1 et classe prévue=0
 
-    :param df:  le dataframe à tester
+    :paramètre 'df': un dataframe à tester
     :return: un dictionnaire incluant les VP,FP,VN,FN,précision et rappel
     """
+    
+    #Initialisation des variables
     VP=0.0
     VN=0.0
     FP=0.0
     FN=0.0
+    
     for i in range(df.count()["target"]):
-        dictI=utils.getNthDict(df,i)
-        tar=dictI["target"]
-        pre=self.estimClass(dictI)
+        #calculer la valeur des variables
+        dictI = utils.getNthDict(df,i)
+        tar = dictI["target"]
+        pre = self.estimClass(dictI)
         if tar==1 and pre==1:
-            VP+=1
+            VP += 1
         if tar==0 and pre==0:
-            VN+=1
+            VN += 1
         if tar==0 and pre==1:
-            FP+=1
+            FP += 1
         if tar==1 and pre==0:
-            FN+=1
+            FN += 1
+            
     return {"VP":VP,"VN":VN,"FP":FP,"FN":FN,"Précision":VP/(VP+FP),"rappel":VP/(VP+FN)}
